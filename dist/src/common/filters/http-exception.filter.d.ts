62bf315e75f2ccefa31629a003c731fe297505b2
vwinv <aminasow98@gmail.com>
2026-01-06 18:41:41 +0000

    reinstallation de nest

---
+++ dist/src/common/filters/http-exception.filter.d.ts
@@ -0,0 +1,6 @@
+import { ExceptionFilter, ArgumentsHost } from '@nestjs/common';
+export declare class AllExceptionsFilter implements ExceptionFilter {
+    private readonly logger;
+    catch(exception: unknown, host: ArgumentsHost): void;
+    private handlePrismaError;
+}
